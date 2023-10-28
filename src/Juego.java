51b556fde07906f2f62316cbc2590649660fca90
BuildTools <unconfigured@null.spigotmc.org>
2023-10-27 22:49:23 -0300

    main

--- src/Juego.java
+++ src/Juego.java
@@ -12,16 +12,18 @@ public class Juego {
         System.out.println(string);
     }
 
-    private static List<Object[]> pokemons = new ArrayList<>();
+    private static List<String[]> pokemons = new ArrayList<>();
 
     public static void getPokemons() throws FileNotFoundException {
         File f = new File("src/pokemoninfo.txt");
         f.setReadable(true);
         Scanner scanFile = new Scanner(f);
         while (scanFile.hasNextLine()) {
-            if (scanFile.nextLine().startsWith("//")) continue;
-            else if (scanFile.nextLine().split(":").length > 1) {
+            String line = scanFile.nextLine();
 
+            if (line.startsWith("//")) continue;
+            else if (line.split(":").length > 1) {
+                pokemons.add(line.split(":"));
             }
         }
     }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,2 +1,22 @@
 public class Pokemon {
+    private String nombre;
+    private String tipo;
+    private int[] stats = new int[3];
+    private String[] mov_rapido;
+    private String[] mov_cargado;
+
+    private int num(String value) {
+        return Integer.valueOf(value);
+    }
+
+    // nombre:tipo:ataque:defensa:hp:mrapidonombre/daño/tipo/energia/segundos:mcargadonombre/daño/tipo/energia
+    public Pokemon(String[] datos) {
+        this.nombre = datos[0];
+        this.tipo = datos[1];
+        this.stats[0] = num(datos[2]);
+        this.stats[1] = num(datos[3]);
+        this.stats[2] = num(datos[4]);
+        this.mov_rapido = datos[5].split("/");
+        this.mov_cargado = datos[6].split("/");
+    }
 }
