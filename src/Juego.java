c4947a99034bac2d41548d9850118196eafa69ce
BuildTools <unconfigured@null.spigotmc.org>
2023-10-27 21:36:19 -0300

    main

--- src/Juego.java
+++ src/Juego.java
@@ -1,6 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Juego {
 
-    public static void main(String[] args) {
+    public static Scanner scanner = new Scanner(System.in);
 
+    public static void println(String string) {
+        System.out.println(string);
+    }
+
+    private static List<Object[]> pokemons = new ArrayList<>();
+
+    public static void getPokemons() throws FileNotFoundException {
+        File f = new File("src/pokemoninfo.txt");
+        f.setReadable(true);
+        Scanner scanFile = new Scanner(f);
+        while (scanFile.hasNextLine()) {
+            if (scanFile.nextLine().startsWith("//")) continue;
+            else if (scanFile.nextLine().split(":").length > 1) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            getPokemons();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
